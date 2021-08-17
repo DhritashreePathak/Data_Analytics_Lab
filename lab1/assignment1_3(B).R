f7326568671eb7ffccc6fdd6905588681dc8f8d9
DhritashreePathak <88723582+DhritashreePathak@users.noreply.github.com>
2021-08-17 20:43:45 +0530

    Create assignment1_3(B).R

---
+++ lab1/assignment1_3(B).R
@@ -0,0 +1,12 @@
+# Q3(B): WAP in R to find display the sum of n natural numbers using function.
+
+{
+  sum <- function(number) {
+    if(number <= 1) {
+      return(number)
+    } else {
+      return(number + sum(number-1))
+    }
+  }
+  sum(8)
+}
